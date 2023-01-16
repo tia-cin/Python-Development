a5133d128a1f63f71c027e6178a55b6b64ae0dd0
cintia arce <cintiaayelenarce@gmail.com>
2023-01-16 16:48:00 -0300

    sql

--- sqlite3.py
+++ sqlite3.py
@@ -1,14 +1,27 @@
+# import db
 import sqlite3
 
+# connect db with file
 connect = sqlite3.connect('client.db')
 
 # connect.execute("DROP TABLE IF EXIST")
+
+# create DB
 # connect.execute('''CREATE TABLE CLIENT (
 #     ID INT PRIMARY KEY NOT NULL, 
 #     NAME TEXT NOT NULL, 
 #     AGE INT NOT NULL
 # );''')
 
+# Insert data in the DB
 connect.execute("INSERT INTO CLIENT (ID,NAME,AGE) VALUES (1, 'Anna', 28)")
+connect.execute("INSERT INTO CLIENT (ID,NAME,AGE) VALUES (2, 'Joe', 46)")
+
+# Get data from DB
+data = connect.execute('''SELECT * FROM CLIENT''')
+
+# Print data
+for row in data:
+    print(row)
 
 connect.close()
