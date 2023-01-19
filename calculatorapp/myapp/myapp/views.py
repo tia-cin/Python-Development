ec71bcf1b675323301ff4e674d58e7da0a1f6c50
cintia arce <cintiaayelenarce@gmail.com>
2023-01-19 14:40:27 -0300

    complete calculator

--- calculatorapp/myapp/myapp/views.py
+++ calculatorapp/myapp/myapp/views.py
@@ -4,9 +4,20 @@ def home(req):
     return render(req, 'home.html')
 
 def result(req):
-    num1 = req.GET.get('num1')
-    num2 = req.GET.get('num2')
+    num1 = int(req.GET.get('num1'))
+    num2 = int(req.GET.get('num2'))
+    operator = req.GET.get('operator')
+    res = 0
 
-    result = num1 + '+' + num2 + '=' + str(int(num1) + int(num2))
+    if(operator == '+'):
+        res = num1 + num2
+    elif(operator == '-'):
+        res = num1 - num2
+    elif(operator == '×'):
+        res = num1 * num2
+    else:
+        res = num1 // num2
+
+    result = str(num1) + operator + str(num2) + '=' + str(res)
 
     return render(req, 'result.html', { 'result' : result })
