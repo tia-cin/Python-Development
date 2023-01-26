ab7363218f8b898877513eac430b1a43b43a142d
cintia arce <cintiaayelenarce@gmail.com>
2023-01-26 14:42:38 -0300

    graph composer - comments

--- projects/graph-composer/compose.py
+++ projects/graph-composer/compose.py
@@ -1,6 +1,6 @@
 import string
 import random
-from graph import Graph, Vertex
+from graph import Graph
 
 def get_text_words(text_path):
     with open(text_path, 'r') as f:
@@ -17,21 +17,28 @@ def make_graph(words):
     prev_word = None
 
     for word in words:
+        # get or add vertex to graph
         word_vertex = g.get_vertex(word)
 
         if prev_word:
+            # word_ vertex is next word
             prev_word.incr_edge(word_vertex)
-        prev_word = word_vertex
+        # set word_vertex if prev is none
+        prev_word = word_vertex 
 
+    # create graph
     g.create_probability_mappings()
     return g
 
 def compose(graph, words, length=50):
     composition = []
+    # set vertex from random word in graph
     word = graph.get_vertex(random.choice(words))
 
     for _ in range(length):
+        # add word to composition
         composition.append(word.val)
+        # update word var to next word
         word = graph.get_next_word(word)
 
     return composition
--- projects/graph-composer/graph.py
+++ projects/graph-composer/graph.py
@@ -3,27 +3,28 @@ import random
 class Vertex:
     def __init__(self, val):
         self.val = val
-        self.adjacent = {}
+        self.adjacent = {} 
         self.neighbors = []
         self.neighbors_weights = []
 
     def add_edge(self, vertex, weight=0):
-        self.adjacent[vertex] = weight
+        self.adjacent[vertex] = weight # { vertex: weight }
 
     def incr_edge(self, vertex):
-        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1
+        # incremente it by one
+        self.adjacent[vertex] = self.adjacent.get(vertex, 0) + 1 
 
     def get_probability_map(self):
         for (vertex, weight) in self.adjacent.items():
-            self.neighbors.append(vertex)
-            self.neighbors_weights.append(weight)
+            self.neighbors.append(vertex) # [...vertexes]
+            self.neighbors_weights.append(weight) # [...weight]
 
     def next_word(self):
         return random.choices(self.neighbors, weights=self.neighbors_weights)[0]
 
 class Graph:
     def __init__(self):
-        self.vertices = {}
+        self.vertices = {} # { value: vertex }
 
     def get_vertex_vals(self):
         return set(self.vertices.keys())
@@ -32,9 +33,10 @@ class Graph:
         self.vertices[val] = Vertex(val)
 
     def get_vertex(self, val):
+        # add value when val not found in vertices
         if val not in self.vertices:
             self.add_vertex(val)
-        return self.vertices[val]
+        return self.vertices[val] # return it
 
     def get_next_word(self, curr_vertex):
         return self.vertices[curr_vertex.val].next_word()
