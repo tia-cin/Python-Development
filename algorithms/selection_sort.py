60221706c8dccac9b3b9e66b56d3ff3e9fc7dcd3
cintia arce <cintiaayelenarce@gmail.com>
2023-01-26 15:23:17 -0300

    selection sort comments

--- algorithms/selection_sort.py
+++ algorithms/selection_sort.py
@@ -2,13 +2,18 @@ import time
 
 def selection_sort(vals):
     start = time.time()
+    # ommit last one
     for i in range(0, len(vals) - 1):
+        # assume curr index is where min value is located
         curr_min_i = i
 
+        # scan all values after next from curr val
         for j in range(i + 1, len(vals)):
+            # update curr_min_i when next val is smaller
             if vals[j] < vals[curr_min_i]:
                 curr_min_i = j
 
+        # switch positions
         vals[i], vals[curr_min_i] = vals[curr_min_i], vals[i]
         print(vals)
     end = time.time()
@@ -18,3 +23,8 @@ def selection_sort(vals):
 ex = [1,5,8,2,4,3,7,6,9]
 
 print(selection_sort(ex))
+
+"""
+    Time: 0.0036628246307373047
+
+"""
