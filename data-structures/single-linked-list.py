c195a78b553cf6922ce9553264f55c515bafe983
cintia arce <cintiaayelenarce@gmail.com>
2023-01-29 16:46:31 -0300

    data structure - single linked list

--- data-structures/single-linked-list.py
+++ data-structures/single-linked-list.py
@@ -6,7 +6,7 @@ class Node:
     
 class SingleLinkedList:
     def __init__(self):
-        self.head = Node()
+        self.head = Node("List")
 
     def add(self, data):
         node = Node(data)
@@ -34,5 +34,66 @@ class SingleLinkedList:
             all_items.append(curr.data)
             curr = curr.next
         
-        return all_items
+        print(all_items)
+
+    def get_item(self, index):
+        if index >= self.length():
+            return "Index out of range!"
+        indx = 0
+        curr = self.head
+
+        while curr.next != None:
+            if indx == index:
+                return curr.data
+
+            indx += 1
+
+    def delete(self, index):
+        if index >= self.length():
+            return "Index out of range!"
+        indx = 0
+        curr = self.head
+
+        while curr.next != None:
+            last = curr
+            curr = curr.next
+            if indx == index:
+                last.next = curr.next
+                return "Deleted!"
+                indx += 1
+
+    def insert(self, index, data):
+        if index >= self.length():
+            return "Index out of range!"
+
+        elif index == self.length():
+            self.add(data)
+
+        node = Node(data)
+        indx = 0
+        curr = self.head
     
+        while curr.next != None:
+            last = curr
+            curr = curr.next
+
+            if indx == index:
+                last.next = node
+                node.next = curr
+            indx += 1
+
+if __name__ == '__main__':
+    l = SingleLinkedList()  
+
+    for num in range(10):
+        l.add(num)
+
+    l.show_list()
+
+    l.get_item(3)
+
+    l.insert(4, 11)
+
+    l.delete(2)
+
+    l.show_list()
