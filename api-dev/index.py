8bb6b6c47bde3b219270b405d4eaa4eee3afb096
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-18 19:49:37 -0300

    server classes

--- api-dev/index.py
+++ api-dev/index.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Comment(BaseModel):
+    user: str
+    comment: str
+    likes: int
+
+class Post(BaseModel):
+    title: str
+    content: str
+    likes: int
+    user: str
+    comments: Comment
+
+
 
 @app.get("/")
 def root():
@@ -12,6 +27,7 @@ def get_posts():
     return {"posts": [1,2,3,4]}
 
 @app.post('/posts')
-def create_post(payload: dict = Body(...)):
+def create_post(new_post: Post):
     print(payload)
     return {"newPost": f"title {payload['title']} content: {payload['content']}"}
+
