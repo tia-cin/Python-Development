aa2079805e1e3d0356c07879e12d6d271351b91e
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-18 19:31:14 -0300

    server methods test

--- api-dev/index.py
+++ api-dev/index.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Hello API"}
+
+@app.get("/")
+def get_posts():
+    return {"posts": [1,2,3,4]}
+
+@app.post('/posts')
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"newPost": f"title {payload['title']} content: {payload['content']}"}
