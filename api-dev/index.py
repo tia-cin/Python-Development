19e471c8eb1f2f4392fa1479b9f5bf6e623ac187
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-19 14:50:39 -0300

    User class & test

--- api-dev/index.py
+++ api-dev/index.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 
 app = FastAPI()
 
@@ -10,8 +10,10 @@ class Comment(BaseModel):
     user: str = "user comment"
     comment: str
     likes: int = 0
+    comment_id: int = 0
 
 class Post(BaseModel):
+    post_id: int = 0
     title: str
     content: str
     likes: int = 0
@@ -20,19 +22,50 @@ class Post(BaseModel):
     private: Optional[bool] = True
     rating: Optional[int] = None
 
+class User(BaseModel):
+    user_id: int = 0
+    username: str
+    email: str
+    followers: Optional[int] = 0
+    following: Optional[int] = 0
+    posts: List[Post] = None
+
+user_posts = [{
+    "title": "TEST 1",
+    "content": "test 1",
+    "likes": 0,
+    "user": "user test 1",
+    "comments": {
+      "user": "user comment 1",
+      "comment": "testing 1",
+      "likes": 0
+    }
+  }, {
+    "title": "TEST 2",
+    "content": "test 2",
+    "likes": 0,
+    "user": "user test 2",
+    "comments": {
+      "user": "user comment 2",
+      "comment": "testing 2",
+      "likes": 0
+    }
+  }]
+
 # GET routes
 @app.get("/")
 def root():
     return {"message": "Hello API"}
 
-@app.get("/")
+@app.get("/posts")
 def get_posts():
-    return {"posts": [1,2,3,4]}
+    return {"posts": user_posts}
 
 
 # POST routes
 @app.post('/posts')
 def create_post(new_post: Post):
+    new_post.dict()
     print(new_post)
-    return {"new post"}
+    return {"new post": new_post}
     # return {"newPost": f"title {new_post['title']} content: {new_post['content']}"}
