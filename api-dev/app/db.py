e0ebe479c1a54892302f325c77ca460b62c6eacd
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-25 11:28:23 -0300

    db get_db fixed

--- api-dev/app/db.py
+++ api-dev/app/db.py
@@ -12,7 +12,7 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    d = SessionLocal()
+    db = SessionLocal()
     try:
         yield db
     finally:
