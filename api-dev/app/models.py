1d215656e5b3b89f275f5b3462cb98b5d418515f
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-25 10:47:17 -0300

    db models

---
+++ api-dev/app/models.py
@@ -0,0 +1,11 @@
+from .db import Base
+from sqlalchemy import Column, ForeignKey, String, Boolean, Integer
+from sqlalchemy.dialects.postgresql import UUID
+
+class Posts(Base):
+    __tablename__ = "posts"
+    id = Column(UUID, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    likes = Column(Integer, default=0)
+    private = Column(Boolean, default=True)
