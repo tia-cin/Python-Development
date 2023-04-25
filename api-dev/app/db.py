01932dd7fa0ce11511a8a3a43e505befdf0bfd79
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-25 10:29:32 -0300

    db setup

---
+++ api-dev/app/db.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# to be changed for security
+SQLALCHEMY_DB_URL = 'postgresql://postgres:data123@localhost/apidev'
+
+engine = create_engine(SQLALCHEMY_DB_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
