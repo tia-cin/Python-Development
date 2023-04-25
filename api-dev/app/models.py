9bbefa311c08d137619ae3ef2861b82c92da9aff
Cintia Arce <cintiaayelenarce@gmail.com>
2023-04-25 11:36:05 -0300

    db models created_At column

--- api-dev/app/models.py
+++ api-dev/app/models.py
@@ -1,11 +1,15 @@
 from .db import Base
 from sqlalchemy import Column, ForeignKey, String, Boolean, Integer
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+from sqlalchemy.sql.expression import text
 
 class Posts(Base):
     __tablename__ = "posts"
+
     id = Column(UUID, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
     likes = Column(Integer, server_default='0')
     private = Column(Boolean, server_default='TRUE')
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
