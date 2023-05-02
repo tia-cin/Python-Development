3596cd25ec62ec01433d646853592e68acf4f352
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-02 11:52:02 -0300

    API-DEV - User Model created

--- api-dev/app/models.py
+++ api-dev/app/models.py
@@ -14,3 +14,11 @@ class Posts(Base):
     likes = Column(Integer, server_default='0')
     private = Column(Boolean, server_default='TRUE')
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+
+class User(Base):
+    __tablename__ = "users"
+
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
+    id = Column(UUID(as_uuid=True), primary_key=True, nullable=False, server_default=text('gen_random_uuid()'))
