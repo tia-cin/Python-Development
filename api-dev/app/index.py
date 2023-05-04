411f621b6023485bc6fb1b3310a6ac010108627f
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-04 15:12:40 -0300

    API-DEV - login process done

--- api-dev/app/index.py
+++ api-dev/app/index.py
@@ -4,7 +4,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from . import models
 from .db import engine
-from .routers import post, user
+from .routers import post, user, auth
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -29,3 +29,4 @@ while True:
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
--- api-dev/app/routers/auth.py
+++ api-dev/app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
-from .. import db, schemas, models
+from .. import db, schemas, models, utils
 
 router = APIRouter(tags=["Authentication"])
 
@@ -8,9 +8,10 @@ router = APIRouter(tags=["Authentication"])
 def login(user_credentials: schemas.UserLogin, db: Session = Depends(db.get_db)):
     user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
 
-    if not user:
+    if not user or not utils.verify:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, 
             detail="Invalid Credential"
         )
-    
+
+    return {"token": "example"}
