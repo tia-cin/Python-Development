6f32f297516b0df6313880aa9f3892a746f4755d
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-04 15:36:36 -0300

    API-DEV - OAuth2 PasswordRequestFrom

--- api-dev/app/routers/auth.py
+++ api-dev/app/routers/auth.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from .. import db, schemas, models, utils, oauth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter(tags=["Authentication"])
 
 @router.post("/auth")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(db.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(db.get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user or not utils.verify:
         raise HTTPException(
