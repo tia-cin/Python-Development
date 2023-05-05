9dfb758424f1a8fec2528a46e5e2234d535de4ff
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-05 13:08:54 -0300

    API-DEV - Assign owner_id to new post

--- api-dev/app/routers/post.py
+++ api-dev/app/routers/post.py
@@ -8,6 +8,8 @@ from uuid import uuid4, UUID
 router = APIRouter(prefix='/posts', tags=["Posts"])
 
 # GET routes
+
+
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     posts = db.query(models.Posts).all()
@@ -16,8 +18,10 @@ def get_posts(db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get
 
 @router.get("/lastest", response_model=List[schemas.Post])
 def get_lastest_posts(db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
-    posts = db.query(models.Posts).order_by(models.Posts.created_at).limit(5).all()
+    posts = db.query(models.Posts).order_by(
+        models.Posts.created_at).limit(5).all()
     return posts
+
 
 @router.get("/public", response_model=List[schemas.Post])
 def get_lastest_posts(db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
@@ -28,7 +32,7 @@ def get_lastest_posts(db: Session = Depends(get_db), curr_user: int = Depends(oa
             status_code=status.HTTP_404_NOT_FOUND,
             detail="Public Posts not found"
         )
-    
+
     return posts
 
 
@@ -45,15 +49,19 @@ def get_post(id: UUID, db: Session = Depends(get_db), curr_user: int = Depends(o
     return post
 
 # POST routes
+
+
 @router.post('/', status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
-    created_post = models.Posts(**new_post.dict())
+def create_post(new_post: schemas.PostCreate, db: Session = Depends(get_db), curr_user: str = Depends(oauth2.get_curr_user)):
+    created_post = models.Posts(owner_id=curr_user.id, **new_post.dict())
     db.add(created_post)
     db.commit()
     db.refresh(created_post)
     return created_post
 
 # DELETE routes
+
+
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: UUID, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     deleted_post = db.query(models.Posts).filter(models.Posts.id == id)
@@ -69,6 +77,8 @@ def delete_post(id: UUID, db: Session = Depends(get_db), curr_user: int = Depend
     db.commit()
 
 # PUT routes
+
+
 @router.put("/{id}", response_model=schemas.Post)
 def update_post(id: UUID, post: schemas.PostCreate, db: Session = Depends(get_db), curr_user: int = Depends(oauth2.get_curr_user)):
     updated_post = db.query(models.Posts).filter(models.Posts.id == id)
