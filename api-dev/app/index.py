ce541db36d20c8bc601fd1606c444935750ec261
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-08 15:22:45 -0300

    API-DEV - Votes Route imported

--- api-dev/app/index.py
+++ api-dev/app/index.py
@@ -1,4 +1,4 @@
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 from fastapi import FastAPI
 from .db import engine
 from . import models
@@ -10,3 +10,4 @@ app = FastAPI()
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
