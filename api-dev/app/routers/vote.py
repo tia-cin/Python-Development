efa8d4582e0c67d04067fc37292f7accb2423d07
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-08 15:29:58 -0300

    API-DEV - Votes Route - check if post exist

--- api-dev/app/routers/vote.py
+++ api-dev/app/routers/vote.py
@@ -7,6 +7,13 @@ router = APIRouter(prefix="/vote", tags=["Vote"])
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 def vote(vote: schemas.Vote, db: Session = Depends(db.get_db), curr_user: str = Depends(oauth2.get_curr_user)):
+    post = db.query(models.Posts).filter(
+        models.Posts.id == vote.post_id).first()
+
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post {vote.post_id} was not found")
+
     query = db.query(models.Vote).filter(models.Vote.post_id ==
                                          vote.post_id, models.Vote.user_id == curr_user.id)
 
