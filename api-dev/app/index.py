062ad2f666920cc0463e1d585607e0a499644e88
Cintia Arce <cintiaayelenarce@gmail.com>
2023-05-10 11:00:04 -0300

    API-DEV - CORS & gitignore file

--- api-dev/app/index.py
+++ api-dev/app/index.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from .routers import post, user, auth, vote
 from fastapi import FastAPI
 from .db import engine
@@ -6,6 +7,11 @@ from . import models
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ['*']
+
+app.add_middleware(CORSMiddleware, allow_origins=origins,
+                   allow_credentials=True, allow_methods=['*'], allow_headers=['*'])
 
 app.include_router(post.router)
 app.include_router(user.router)
